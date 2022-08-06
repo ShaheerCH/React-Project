edb45e79445321f8b51452521482309b41a7b35a
shaheer ch <shaheer.chry@gmail.com>
2022-08-06 16:11:08 +0500

    Final Edit

---
+++ src/components/NavBar/Nav.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import Nav from './Nav'
+
+describe('Jest test', () => {
+  it('Match Snapshot', () => {
+    const { container } = render(
+      <BrowserRouter>
+        <Nav />
+      </BrowserRouter>,
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
